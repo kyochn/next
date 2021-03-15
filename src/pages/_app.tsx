91b9e415cf21345e5478a74a1f27d9a442ad4691
kyochn <is0586vi@ed.ritsumei.ac.jp>
2021-03-15 17:59:20 +0900

    Appコンポーネントを変更

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import '../styles/globals.css'
-
-function MyApp({ Component, pageProps }) {
+import React from 'react'
+import {AppProps} from 'next/app'
+ 
+function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return <Component {...pageProps} />
 }
 
