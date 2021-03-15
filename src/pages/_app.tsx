7e42b6dd02b8addb1bb142082eccc48268eefd2b
kyochn <is0586vi@ed.ritsumei.ac.jp>
2021-03-15 20:51:22 +0900

    ベースURLを追記

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import '../styles/globals.css'
+import 'styles/globals.css'
 import React from 'react'
 import {AppProps} from 'next/app'
  
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import styles from 'styles/Home.module.css'
 // React と NextPage を読み込む
 import React from 'react'
 import { NextPage } from 'next'
