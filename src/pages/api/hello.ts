82633ecff00153ba12f4cd97a7902f817860014e
kyochn <is0586vi@ed.ritsumei.ac.jp>
2021-03-15 18:36:08 +0900

    APIをTSに対応させた

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,5 +1,17 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default (req, res) => {
+import express from 'express'
+const app: express.Express = express()
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+// レスポンスの型を追加
+type Response = {
+  statusCode: number
+  json({ name: string }): void
+}
+
+// 型を指定＆使用していない引数にアンダースコア接頭詞を追加
+export default (_req: void, res: express.Response): void => {
   res.status(200).json({ name: 'John Doe' })
 }
