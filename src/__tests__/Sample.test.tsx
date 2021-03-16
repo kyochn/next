ed49a5949d5896cb5c4c196d8ad1fb5d5f0952a7
kyochn <is0586vi@ed.ritsumei.ac.jp>
2021-03-16 16:19:28 +0900

    ReactTestingLibraryを追加

--- src/__tests__/Sample.test.tsx
+++ src/__tests__/Sample.test.tsx
@@ -2,6 +2,17 @@
 
 import React from 'react'
 import Home from 'pages/index'
+import { cleanup, render, screen } from '@testing-library/react'
+
+// 各テスト実行後にレンダーしたコンポーネントをアンマウントする
+afterEach(cleanup)
+
+it('「Next.js!」のリンクが Next.js の公式サイトのトップページである', () => {
+  render(<Home />)
+  expect(screen.getByText('Next.js!').getAttribute('href')).toBe(
+    'https://nextjs.org'
+  )
+})
 
 it('Home ページコンポーネントが存在している', () => {
   expect(Home).toBeTruthy()
