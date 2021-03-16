5bbf3389b3c506c5bf69b67c7721882229f2173a
kyochn <is0586vi@ed.ritsumei.ac.jp>
2021-03-16 14:12:23 +0900

    Appコンポーネントでsanitize.cssを読み込むようにした

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import 'styles/globals.scss'
 import React from 'react'
 import {AppProps} from 'next/app'
+import 'sanitize.css'
  
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return <Component {...pageProps} />
