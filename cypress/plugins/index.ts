33d24688d58d5f2194e2c357c4578c20e36ae0ab
kyochn <is0586vi@ed.ritsumei.ac.jp>
2021-03-16 19:36:14 +0900

    Cypressでテストできるようにした

---
+++ cypress/plugins/index.ts
@@ -0,0 +1,28 @@
+/// <reference types="cypress" />
+// ***********************************************************
+// This example plugins/index.js can be used to load plugins
+//
+// You can change the location of this file or turn off loading
+// the plugins file with the 'pluginsFile' configuration option.
+//
+// You can read more here:
+// https://on.cypress.io/plugins-guide
+// ***********************************************************
+
+// This function is called when a project is opened or re-opened (e.g. due to
+// the project's config changing)
+
+/**
+ * @type {Cypress.PluginConfig}
+ */
+// eslint-disable-next-line no-unused-vars
+module.exports = (on, config) => {
+  // `on` is used to hook into various events Cypress emits
+  // `config` is the resolved Cypress config
+}
+
+// 使っていない引数を削除して ES Modules の記述に変更
+export default (): void => {
+  // `on` is used to hook into various events Cypress emits
+  // `config` is the resolved Cypress config
+}
